7f55493d8fb20174a2a30367793a9985c3f8ea9f
LeeMyungdeok <115915362+LeeMyungdeok@users.noreply.github.com>
2023-09-30 14:27:59 +0900

    Update secret.py

--- AutomaticTrading/secret.py
+++ AutomaticTrading/secret.py
@@ -14,37 +14,3 @@
 # ㄹㅇ 5번째 도전 조현준 계좌라서 안되나....
 # CANO = "30081099"
 
-#실전계좌
-CANO = "63579086"
-
-# 계좌번호 뒤 2자리
-ACNT_PRDT_CD = "01"
-
-#실전 투자 계좌
-APP_KEY = "PSendIJK8uMw8YWoao1Y1gHWmeJQrKqHJqgK"
-APP_SECRET = "kCLX1J+pTUUElRgXzvKgOApm/ywFgp2s3TPBzuAPBArPBFp/y9plUtVnVWz7AmnOF9Kq9XsoJ/igZmCOpgdR/vihDAGUwVUJWCkUsj3CfwLB5nClLxP2cueHj16nPnckA8xjsuZJ9a4igqtFjV6eQPZFts3WH85Q3W8ebSwFzJNzJPU1mF0="
-
-
-
-# 계좌번호 앞 8자리 조현준꺼라서 안되나 한투가 막고있나 화나는중
-# ㄹㅇ 5번째 도전 조현준 계좌라서 안되나....
-# CANO = "30081099"
-
-#실전계좌
-CANO = "63579086"
-
-# 계좌번호 뒤 2자리
-ACNT_PRDT_CD = "01"
-
-
-# 실전투자
-URL_BASE = "https://openapi.koreainvestment.com:9443"
-# 모의투자
-# URL_BASE = "https://openapivts.koreainvestment.com:29443"
-
-ACCESS_TOKEN = ""
-HASHKEY= ""
-AMOUNT = 0
-
-# 종목코드 AAPL = 애플 / TSLA = 테슬라
-ticker = "AAPL"
