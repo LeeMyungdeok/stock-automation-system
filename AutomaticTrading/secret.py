d7675e1eec8b07f5ecb8a237331ba8bfb6f18748
LeeMyungdeok <115915362+LeeMyungdeok@users.noreply.github.com>
2023-09-30 14:30:40 +0900

    Update secret.py

--- AutomaticTrading/secret.py
+++ AutomaticTrading/secret.py
@@ -5,12 +5,9 @@
 
 # 홈페이지에서 API서비스 신청시 받은 Appkey, Appsecret 값 설정
 #모의투자 계좌
-# APP_KEY = "PSixQBljUVzYjG3YhUtPon6os5DQ1colz4y5"
-# APP_SECRET = "r4fWgOhD6P2kjNLEHfCtV5ULtAHUFA9seT+CFwOmBi5VGhJYE90+XYV4sdY7WfhhUVVoMzSIJM6bP7ltKZOlessopPYLIeLKzr7b8V29Y9gR2Nyr7EIviWNRIKmfc9tElyym4LO5Ev6uA2eB8tPNrtR77OVJloaz4yN6DHA37QjmB2+aGxI="
 
 
 
 # 계좌번호 앞 8자리 조현준꺼라서 안되나 한투가 막고있나 화나는중
-# ㄹㅇ 5번째 도전 조현준 계좌라서 안되나....
-# CANO = "30081099"
+
 
